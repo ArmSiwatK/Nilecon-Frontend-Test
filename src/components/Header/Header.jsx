6dc3759cc36cc34a78a1cff2028c493a3dc0ab6a
ArmSiwatK <arm.siwatk@gmail.com>
2023-07-21 18:34:41 +0700

    feat: Header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import "./Header.scss";
+
+const Header = () => {
+    return (
+        <header>
+            <div className="left-side">
+                <span>Follow us on</span>
+                <a href="https://www.facebook.com/embassycineplex" target="_blank" rel="noopener noreferrer">
+                    <img src="./images/icon-top-fb.png" alt="Facebook" />
+                </a>
+                <a href="https://twitter.com/embassycineplex" target="_blank" rel="noopener noreferrer">
+                    <img src="./images/icon-top-tw.png" alt="Twitter" />
+                </a>
+                <a href="https://www.instagram.com/embassyscreens" target="_blank" rel="noopener noreferrer">
+                    <img src="./images/icon-top-ig.png" alt="Instagram" />
+                </a>
+                <a href="https://www.youtube.com/user/embassycineplex" target="_blank" rel="noopener noreferrer">
+                    <img src="./images/icon-top-yt.png" alt="YouTube" />
+                </a>
+            </div>
+            <div className="right-side">
+                <span>Get Embassy Screens app on</span>
+                <a href="https://apps.apple.com/app/embassy-screens/id882209348" target="_blank" rel="noopener noreferrer">
+                    <img src="./images/download-appstore.png" alt="App Store" />
+                </a>
+                <a href="https://play.google.com/store/apps/details?id=th.co.invp.EmbassyActivity" target="_blank" rel="noopener noreferrer">
+                    <img src="./images/download-googleplay.png" alt="Google Play" />
+                </a>
+            </div>
+        </header>
+    );
+};
+
+export default Header;
