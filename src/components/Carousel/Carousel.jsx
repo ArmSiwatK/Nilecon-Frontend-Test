c529d13e9932df5b0467f37c092211d4cbd68405
ArmSiwatK <arm.siwatk@gmail.com>
2023-07-22 15:53:07 +0700

    feat: Carousel done

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Carousel from "react-multi-carousel";
 import carouselData from '../../assets/CarouselData.json';
 import "react-multi-carousel/lib/styles.css";
@@ -12,16 +12,38 @@ const BannerCarousel = () => {
         }
     };
 
+    const CustomLeftArrow = ({ onClick }) => {
+        return (
+            <div className="custom-arrow left" onClick={() => onClick()}>
+                <img src="./images/btn-prev.png" alt="Carousel Previous Button" />
+            </div>
+        );
+    };
+
+    const CustomRightArrow = ({ onClick }) => {
+        return (
+            <div className="custom-arrow right" onClick={() => onClick()}>
+                <img src="./images/btn-next.png" alt="Carousel Next Button" />
+            </div>
+        );
+    };
+
     return (
         <Carousel
             responsive={responsive}
             infinite={true}
+            customLeftArrow={<CustomLeftArrow />}
+            customRightArrow={<CustomRightArrow />}
         >
-            <img className="banner" src={"./banners/banner1.jpg"} />
-            <img className="banner" src={"./banners/banner2.jpg"} />
-            <img className="banner" src={"./banners/banner3.jpg"} />
-            <img className="banner" src={"./banners/banner4.jpg"} />
-            <img className="banner" src={"./banners/banner5.jpg"} />
+            {carouselData.map((item, index) => (
+                item.url ? (
+                    <a key={index} href={item.url} target="_blank" rel="noopener noreferrer">
+                        <img className="banner" src={item.imageSrc} alt={`Banner ${index + 1}`} />
+                    </a>
+                ) : (
+                    <img key={index} className="banner" src={item.imageSrc} alt={`Banner ${index + 1}`} />
+                )
+            ))}
         </Carousel>
     );
 };
