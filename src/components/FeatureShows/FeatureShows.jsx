cbbeff8de0950eb8b7d42ea6246585d7e97f9f0a
ArmSiwatK <arm.siwatk@gmail.com>
2023-07-23 16:34:20 +0700

    feat: Feature show information side

--- src/components/FeatureShows/FeatureShows.jsx
+++ src/components/FeatureShows/FeatureShows.jsx
@@ -1,9 +1,51 @@
 import React from 'react';
+import movieData from '../../assets/MovieData.json';
 import './FeatureShows.scss';
 
 const FeatureShows = () => {
+    const currentTime = new Date();
+    const currentMinutes = currentTime.getHours() * 60 + currentTime.getMinutes();
+
+    const isScreenTimePast = (screenTime) => {
+        const [hours, minutes] = screenTime.split(':').map(Number);
+        const screenMinutes = hours * 60 + minutes;
+        return screenMinutes < currentMinutes;
+    };
+
     return (
-        <div className="feature-shows-title">SHOWTIMES</div>
+        <div className="feature-shows">
+            <div className="feature-shows-title">SHOWTIMES</div>
+            <div className="feature-shows-container">
+                <div className="feature-show-block">
+                    <a className="movie-cover-link">
+                        <img className="movie-cover" src={movieData.movieCover} alt={movieData.movieName} />
+                    </a>
+                    <div className="movie-info">
+                        <div className="movie-name">{movieData.movieName.toUpperCase()}</div>
+                        <div className="movie-time">
+                            <img className="clock-icon" src="./images/clock.png" />
+                            <div className="movie-length">{movieData.movieLength} MIN</div>
+                        </div>
+                        <div className="movie-info-icons">
+                            <img className="movie-rating" src="./images/rate-general.png" />
+                            <img className="clock-movie-type" src="./images/type-digital.png" />
+                        </div>
+                    </div>
+                </div>
+                <div className="screen-time-container">
+                    {movieData.screenTime.map((screen, index) => {
+                        const boxId = isScreenTimePast(screen.time) ? 'past' : 'future';
+                        return (
+                            <div key={index} className="screen-time-box" id={`${boxId}`}>
+                                <div className="hall-type">{`Hall ${screen.hall}`}</div>
+                                <div className="screen-time">{screen.time}</div>
+                                <div className="movie-type">{movieData.movieType}</div>
+                            </div>
+                        );
+                    })}
+                </div>
+            </div>
+        </div>
     );
 };
 
