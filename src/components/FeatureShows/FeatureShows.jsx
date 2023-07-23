cdf48b8a590f0016dc35dc9753d13c5c88107e8b
ArmSiwatK <arm.siwatk@gmail.com>
2023-07-23 16:38:07 +0700

    style: Shows list framing

--- src/components/FeatureShows/FeatureShows.jsx
+++ src/components/FeatureShows/FeatureShows.jsx
@@ -15,34 +15,36 @@ const FeatureShows = () => {
     return (
         <div className="feature-shows">
             <div className="feature-shows-title">SHOWTIMES</div>
-            <div className="feature-shows-container">
-                <div className="feature-show-block">
-                    <a className="movie-cover-link">
-                        <img className="movie-cover" src={movieData.movieCover} alt={movieData.movieName} />
-                    </a>
-                    <div className="movie-info">
-                        <div className="movie-name">{movieData.movieName.toUpperCase()}</div>
-                        <div className="movie-time">
-                            <img className="clock-icon" src="./images/clock.png" />
-                            <div className="movie-length">{movieData.movieLength} MIN</div>
-                        </div>
-                        <div className="movie-info-icons">
-                            <img className="movie-rating" src="./images/rate-general.png" />
-                            <img className="clock-movie-type" src="./images/type-digital.png" />
+            <div className="feature-shows-list">
+                <div className="feature-shows-container">
+                    <div className="feature-show-block">
+                        <a className="movie-cover-link">
+                            <img className="movie-cover" src={movieData.movieCover} alt={movieData.movieName} />
+                        </a>
+                        <div className="movie-info">
+                            <div className="movie-name">{movieData.movieName.toUpperCase()}</div>
+                            <div className="movie-time">
+                                <img className="clock-icon" src="./images/clock.png" />
+                                <div className="movie-length">{movieData.movieLength} MIN</div>
+                            </div>
+                            <div className="movie-info-icons">
+                                <img className="movie-rating" src="./images/rate-general.png" />
+                                <img className="clock-movie-type" src="./images/type-digital.png" />
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div className="screen-time-container">
-                    {movieData.screenTime.map((screen, index) => {
-                        const boxId = isScreenTimePast(screen.time) ? 'past' : 'future';
-                        return (
-                            <div key={index} className="screen-time-box" id={`${boxId}`}>
-                                <div className="hall-type">{`Hall ${screen.hall}`}</div>
-                                <div className="screen-time">{screen.time}</div>
-                                <div className="movie-type">{movieData.movieType}</div>
-                            </div>
-                        );
-                    })}
+                    <div className="screen-time-container">
+                        {movieData.screenTime.map((screen, index) => {
+                            const boxId = isScreenTimePast(screen.time) ? 'past' : 'future';
+                            return (
+                                <div key={index} className="screen-time-box" id={`${boxId}`}>
+                                    <div className="hall-type">{`Hall ${screen.hall}`}</div>
+                                    <div className="screen-time">{screen.time}</div>
+                                    <div className="movie-type">{movieData.movieType}</div>
+                                </div>
+                            );
+                        })}
+                    </div>
                 </div>
             </div>
         </div>
