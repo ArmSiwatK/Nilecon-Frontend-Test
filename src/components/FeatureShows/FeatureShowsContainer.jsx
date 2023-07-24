489f17ffc12dd8db78ac55e8a8a0d5c8fbfd0c38
ArmSiwatK <arm.siwatk@gmail.com>
2023-07-24 18:59:38 +0700

    feat: Use react-tooltip

--- src/components/FeatureShows/FeatureShowsContainer.jsx
+++ src/components/FeatureShows/FeatureShowsContainer.jsx
@@ -1,16 +1,7 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Tooltip } from 'react-tooltip';
 
 const FeatureShowsContainer = ({ movieData, isScreenTimePast, currentScreenTimeIndex }) => {
-    const [currentHoverIndex, setCurrentHoverIndex] = useState(null);
-
-    const handleMouseEnter = (index) => {
-        setCurrentHoverIndex(index);
-    };
-
-    const handleMouseLeave = () => {
-        setCurrentHoverIndex(null);
-    };
-
     return (
         <div className="feature-show-container">
             <div className="feature-shows-container">
@@ -44,28 +35,26 @@ const FeatureShowsContainer = ({ movieData, isScreenTimePast, currentScreenTimeI
 
                         return (
                             <div
-                                key={index}
-                                className="screen-time-box"
-                                id={currentScreenTimeIndex === index ? 'current' : boxId}
-                                onMouseEnter={() => handleMouseEnter(index)}
-                                onMouseLeave={handleMouseLeave}
+                                data-tooltip-id="message-box"
+                                data-tooltip-content={`Audio: ${movieData.movieAudio} / Subtitles: ${movieData.movieSubtitles}`}
                             >
-                                <div className="hall-type" style={hallTypeStyle}></div>
-                                <a>
-                                    <div className="screen-time">{screen.time}</div>
-                                </a>
-                                <div className="movie-type">{movieData.movieType}</div>
-
-                                {currentHoverIndex === index && (
-                                    <div className="message-box">
-                                        Audio: {movieData.movieAudio} / Subtitles: {movieData.movieSubtitles}
-                                    </div>
-                                )}
+                                <div
+                                    key={index}
+                                    className="screen-time-box"
+                                    id={currentScreenTimeIndex === index ? 'current' : boxId}
+                                >
+                                    <div className="hall-type" style={hallTypeStyle}></div>
+                                    <a>
+                                        <div className="screen-time">{screen.time}</div>
+                                    </a>
+                                    <div className="movie-type">{movieData.movieType}</div>
+                                </div>
                             </div>
                         );
                     })}
                 </div>
             </div>
+            <Tooltip id="message-box" />
         </div>
     );
 };
