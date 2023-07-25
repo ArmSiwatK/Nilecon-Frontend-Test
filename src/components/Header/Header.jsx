5de567f63ee15fc9f546c375469ae9c662581df5
ArmSiwatK <arm.siwatk@gmail.com>
2023-07-25 13:06:15 +0700

    style: Responsive logo

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,34 +1,59 @@
-import React from 'react';
-import "./Header.scss";
+import React, { useState, useEffect } from 'react';
+import './Header.scss';
 
 const Header = () => {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 1100);
+        };
+
+        handleResize();
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
-        <header>
-            <div className="header-left">
-                <span>Follow us on</span>
-                <a href="https://www.facebook.com/embassycineplex" target="_blank" rel="noopener noreferrer">
-                    <img src="./icons/icon-fb.png" alt="Facebook" />
-                </a>
-                <a href="https://twitter.com/embassycineplex" target="_blank" rel="noopener noreferrer">
-                    <img src="./icons/icon-tw.png" alt="Twitter" />
-                </a>
-                <a href="https://www.instagram.com/embassyscreens" target="_blank" rel="noopener noreferrer">
-                    <img src="./icons/icon-ig.png" alt="Instagram" />
-                </a>
-                <a href="https://www.youtube.com/user/embassycineplex" target="_blank" rel="noopener noreferrer">
-                    <img src="./icons/icon-yt.png" alt="YouTube" />
-                </a>
-            </div>
-            <div className="header-right">
-                <span>Get Embassy Screens app on</span>
-                <a href="https://apps.apple.com/app/embassy-screens/id882209348" target="_blank" rel="noopener noreferrer">
-                    <img src="./images/download-appstore.png" alt="App Store" />
-                </a>
-                <a href="https://play.google.com/store/apps/details?id=th.co.invp.EmbassyActivity" target="_blank" rel="noopener noreferrer">
-                    <img src="./images/download-googleplay.png" alt="Google Play" />
-                </a>
-            </div>
-        </header>
+        <>
+            {isMobile ? (
+                <header>
+                    <a className="mobile-logo" href="https://www.embassycineplex.com/home">
+                        <img src="./images/logo-mobile.svg" alt="Logo" />
+                    </a>
+                </header>
+            ) : (
+                <header>
+                    <div className="header-left">
+                        <span>Follow us on</span>
+                        <a href="https://www.facebook.com/embassycineplex" target="_blank" rel="noopener noreferrer">
+                            <img src="./icons/icon-fb.png" alt="Facebook" />
+                        </a>
+                        <a href="https://twitter.com/embassycineplex" target="_blank" rel="noopener noreferrer">
+                            <img src="./icons/icon-tw.png" alt="Twitter" />
+                        </a>
+                        <a href="https://www.instagram.com/embassyscreens" target="_blank" rel="noopener noreferrer">
+                            <img src="./icons/icon-ig.png" alt="Instagram" />
+                        </a>
+                        <a href="https://www.youtube.com/user/embassycineplex" target="_blank" rel="noopener noreferrer">
+                            <img src="./icons/icon-yt.png" alt="YouTube" />
+                        </a>
+                    </div>
+                    <div className="header-right">
+                        <span>Get Embassy Screens app on</span>
+                        <a href="https://apps.apple.com/app/embassy-screens/id882209348" target="_blank" rel="noopener noreferrer">
+                            <img src="./images/download-appstore.png" alt="App Store" />
+                        </a>
+                        <a href="https://play.google.com/store/apps/details?id=th.co.invp.EmbassyActivity" target="_blank" rel="noopener noreferrer">
+                            <img src="./images/download-googleplay.png" alt="Google Play" />
+                        </a>
+                    </div>
+                </header>
+            )}
+        </>
     );
 };
 
--- src/components/Logo/Logo.jsx
+++ src/components/Logo/Logo.jsx
@@ -1,13 +1,30 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './Logo.scss';
 
 const Logo = () => {
+    const [isMobile, setIsMobile] = useState(false);
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 1100);
+        };
+
+        handleResize();
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
-        <div className="logo-wrapper">
-            <a className="logo" href="https://www.embassycineplex.com/home">
-                <img src="./images/logo.png" alt="Logo" />
-            </a>
-        </div>
+        !isMobile && (
+            <div className="logo-wrapper">
+                <a className="logo" href="https://www.embassycineplex.com/home">
+                    <img src="./images/logo.png" alt="Logo" />
+                </a>
+            </div>
+        )
     );
 };
 
