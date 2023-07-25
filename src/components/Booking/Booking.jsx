3e998a2913a685900eae0ae0388723d855d2ba4a
ArmSiwatK <arm.siwatk@gmail.com>
2023-07-25 19:53:10 +0700

    feat: Movie info

--- src/components/Booking/Booking.jsx
+++ src/components/Booking/Booking.jsx
@@ -3,14 +3,34 @@ import movieData from '../../assets/MovieData.json';
 import './Booking.scss';
 
 const Booking = () => {
+    const today = new Date();
+
+    const getMonthName = (monthIndex) => {
+        const monthNames = [
+            "January", "February", "March", "April", "May", "June",
+            "July", "August", "September", "October", "November", "December"
+        ];
+        return monthNames[monthIndex];
+    };
+
+    const formattedDate = () => {
+        const day = today.getDate();
+        const monthIndex = today.getMonth();
+        const year = today.getFullYear();
+
+        return `${day} ${getMonthName(monthIndex)} ${year}`;
+    };
+
     const handleGoBack = () => {
         window.history.back();
     };
 
+    const lastScreenTime = movieData.screenTime[movieData.screenTime.length - 1];
+
     return (
         <div className="booking-container">
             <div className="back-button">
-                <input type="image" src="./images/btn-back.png" onClick={handleGoBack} />
+                <input type="image" src="./images/btn-back.png" onClick={handleGoBack} alt="Back" />
             </div>
             <div className="booking-info">
                 <img className="movie-cover" src={movieData.movieCover} alt={movieData.movieName} />
@@ -20,6 +40,11 @@ const Booking = () => {
                         <img className="movie-rating" src="./images/rate-general.png" />
                         <img className="clock-movie-type" src="./images/type-digital.png" />
                         <div className="movie-type">{movieData.movieType}</div>
+                    </div>
+                    <div className="movie-details">
+                        <div className="movie-details-cell"><span>Date:</span> {formattedDate()}</div>
+                        <div className="movie-details-cell"><span>Time:</span> {lastScreenTime.time}</div>
+                        <div className="movie-details-cell"><span>Hall:</span> {lastScreenTime.hall}</div>
                     </div>
                 </div>
             </div>
