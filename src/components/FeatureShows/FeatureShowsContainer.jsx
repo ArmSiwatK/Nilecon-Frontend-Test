519957f00d969deecdc7b428d10b3285de392955
ArmSiwatK <arm.siwatk@gmail.com>
2023-07-26 11:38:51 +0700

    feat: Conditional navigation
    If the current time compared to the movie's screening time is less than an hour, the user can't navigate

--- src/components/FeatureShows/FeatureShowsContainer.jsx
+++ src/components/FeatureShows/FeatureShowsContainer.jsx
@@ -3,6 +3,17 @@ import { NavLink } from "react-router-dom";
 import { Tooltip } from 'react-tooltip';
 
 const FeatureShowsContainer = ({ movieData, isScreenTimePast, currentScreenTimeIndex }) => {
+    const currentTime = new Date();
+
+    const canNavigate = (screenTime) => {
+        const screenTimeDate = new Date();
+        const [hours, minutes] = screenTime.split(':');
+        screenTimeDate.setHours(hours, minutes, 0, 0);
+
+        const timeDifference = screenTimeDate.getTime() - currentTime.getTime();
+        return timeDifference >= 60 * 60 * 1000;
+    };
+
     return (
         <div className="feature-show-container">
             <div className="feature-shows-container">
@@ -29,20 +40,27 @@ const FeatureShowsContainer = ({ movieData, isScreenTimePast, currentScreenTimeI
                             backgroundImage: `url(./images/hilight-hall-${screen.hall}.png)`,
                         };
 
-
                         if (boxId === 'future' && currentScreenTimeIndex === null) {
                             currentScreenTimeIndex = index;
                         }
+
+                        const shouldNavigate = canNavigate(screen.time);
 
                         return (
                             <div
                                 data-tooltip-id="message-box"
                                 data-tooltip-content={`Audio: ${movieData.movieAudio} / Subtitles: ${movieData.movieSubtitles}`}
                             >
-                                <NavLink to="/select-ticket"
+                                <NavLink
+                                    to={shouldNavigate ? "/select-ticket" : "#"}
                                     key={index}
                                     className="screen-time-box"
                                     id={currentScreenTimeIndex === index ? 'current' : boxId}
+                                    onClick={(event) => {
+                                        if (!shouldNavigate) {
+                                            event.preventDefault();
+                                        }
+                                    }}
                                 >
                                     <div className="hall-type" style={hallTypeStyle}></div>
                                     <a>
