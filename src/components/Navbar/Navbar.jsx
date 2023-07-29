84a5f71f30ee6940fe926eb46f27b4703242ae39
ArmSiwatK <arm.siwatk@gmail.com>
2023-07-29 19:47:22 +0700

    style: Responsive Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { NavLink } from "react-router-dom";
 import NavItems from '../../assets/NavItems.json';
 import './Navbar.scss';
@@ -6,6 +6,7 @@ import './Navbar.scss';
 const Navbar = () => {
     const [language, setLanguage] = useState('EN');
     const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [isMobile, setIsMobile] = useState(false);
     const languages = ['EN', 'TH'];
 
     const handleLanguageChange = (selectedLanguage) => {
@@ -17,32 +18,57 @@ const Navbar = () => {
         setDropdownOpen(!dropdownOpen);
     };
 
+    useEffect(() => {
+        const handleResize = () => {
+            setIsMobile(window.innerWidth <= 1100);
+        };
+
+        handleResize();
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
+
     return (
         <div className="navbar">
-            {NavItems.map((item) => (
-                <NavLink
-                    to={item.path}
-                    key={item.displayName}
-                    className="nav-item"
-                >
-                    {item.displayName}
-                </NavLink>
-            ))}
-            <div className={`language-dropdown ${dropdownOpen ? 'open' : ''}`} onClick={handleDropdownClick}>
-                <span className="selected-language">{language}</span>
-                <span className="dropdown-icon">{dropdownOpen ? '⏶' : '⏷'}</span>
-                {dropdownOpen && (
-                    <div className="dropdown-content">
-                        {languages
-                            .filter((lang) => lang !== language)
-                            .map((lang) => (
-                                <div key={lang} onClick={() => handleLanguageChange(lang)}>
-                                    {lang}
-                                </div>
-                            ))}
-                    </div>
-                )}
-            </div>
+            {isMobile
+                ? NavItems.slice(0, 3).map((item) => (
+                    <NavLink
+                        to={item.path}
+                        key={item.displayName}
+                        className="nav-item"
+                    >
+                        {item.displayName}
+                    </NavLink>
+                ))
+                : NavItems.map((item) => (
+                    <NavLink
+                        to={item.path}
+                        key={item.displayName}
+                        className="nav-item"
+                    >
+                        {item.displayName}
+                    </NavLink>
+                ))}
+            {!isMobile && (
+                <div className={`language-dropdown ${dropdownOpen ? 'open' : ''}`} onClick={handleDropdownClick}>
+                    <span className="selected-language">{language}</span>
+                    <span className="dropdown-icon">{dropdownOpen ? '⏶' : '⏷'}</span>
+                    {dropdownOpen && (
+                        <div className="dropdown-content">
+                            {languages
+                                .filter((lang) => lang !== language)
+                                .map((lang) => (
+                                    <div key={lang} onClick={() => handleLanguageChange(lang)}>
+                                        {lang}
+                                    </div>
+                                ))}
+                        </div>
+                    )}
+                </div>
+            )}
         </div>
     );
 };
